e5c961a3f767340c9bb9b7a3dccd2c4683e1a533
Anton Krylov <anthony.kryloff@gmail.com>
2018-04-16 21:20:58 +0300

    fix liquibase; add heroku configs

--- src/main/java/ru/kpfu/itis/csport/config/PersistenceConfig.java
+++ src/main/java/ru/kpfu/itis/csport/config/PersistenceConfig.java
@@ -1,6 +1,7 @@
 package ru.kpfu.itis.csport.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,17 +13,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
-import java.io.IOException;
 
 @Configuration
 @PropertySource("classpath:persistence.properties")
+@PropertySource(value = "classpath:application-heroku.properties", ignoreResourceNotFound = true)
 @EnableJpaRepositories("ru.kpfu.itis.csport.repository")
 @EnableTransactionManagement
 public class PersistenceConfig implements EnvironmentAware {
@@ -35,11 +35,11 @@ public class PersistenceConfig implements EnvironmentAware {
     }
 
     @Bean
-    public DataSource dataSource() throws PropertyVetoException, IOException {
-        String driver = env.getProperty("jdbc.driver");
-        String url = env.getProperty("jdbc.url");
-        String user = env.getProperty("jdbc.user");
-        String password = env.getProperty("jdbc.password");
+    public DataSource dataSource() throws PropertyVetoException {
+        String driver = env.getProperty("spring.datasource.driver-class-name");
+        String url = env.getProperty("spring.datasource.url");
+        String user = env.getProperty("spring.datasource.username");
+        String password = env.getProperty("spring.datasource.password");
 
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setDriverClass(driver);
@@ -50,13 +50,11 @@ public class PersistenceConfig implements EnvironmentAware {
     }
 
     @Bean
-    public EntityManagerFactory entityManagerFactory() throws PropertyVetoException, IOException {
+    public EntityManagerFactory entityManagerFactory() throws PropertyVetoException {
         // Jpa vendor adapter
         HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-        jpaVendorAdapter.setDatabase(Database.POSTGRESQL);
         jpaVendorAdapter.setShowSql(true);
         jpaVendorAdapter.setGenerateDdl(true);
-        jpaVendorAdapter.setDatabasePlatform(env.getProperty("hibernate.dialect"));
 
         // Entity manager factory
         LocalContainerEntityManagerFactoryBean entityManagerFactory =
@@ -84,6 +82,14 @@ public class PersistenceConfig implements EnvironmentAware {
     @Bean
     public PersistenceAnnotationBeanPostProcessor persistenceAnnotationBean() {
         return new PersistenceAnnotationBeanPostProcessor();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:/db/changelog.xml");
+        return liquibase;
     }
 
 }
